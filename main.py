43404e5b6a3d62a3cc019239289ca79c39c55c33
Justin Gries <justin.gries@gmail.com>
2021-12-07 09:48:33 -0800

    Project start

---
+++ main.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+'''
+TODO: User Functionality:
+    TODO: Design Interface:
+        TODO: Option to make a new first class or coach reservation
+            NOTE: Only 8 seats are available at $500
+            NOTE: Only 40 seats are available at $199
+            TODO: If the reservation attendant picks first class or coach reservation,
+                TODO: Prompt attendant for the location of the desired seat
+                    TODO: If not available, it will be refused
+                TODO: Prompt attendant for the name of the person taking the flight.
+
+        TODO: change an existing reservation
+        TODO: print the listing of seats
+            TODO: the system will print in an orderly manner all the seats available in each compartment and the list of
+                seats that are taken with the individual that has reserved the seat
+            TODO: If all seats are taken a message is displayed stating, no new reservations can be made
+            TODO: If the person’s name is longer than 12 characters you will only display the first 12 characters
+                Note: the entire name should be in the array, you only to reduce the length of text during the print
+                function for neatness.
+        TODO: quit
+
+'''
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
