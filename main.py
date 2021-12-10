03b7ad711f5bbdead60793bbc92a556500cffea6
Justin Gries <justin.gries@gmail.com>
2021-12-10 12:31:02 -0800

    named module more appropriately got rid of unnecessary files

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-'''
+"""
 Guidance found in flight_reservation_system.docx, provided
 Refer to guidance for table and text formatting
-    Print Header
-    Instantiate a MainMenu instance to handle main menu functionality
-'''
+"""
 
-from bookingdata import run_reservation_system_pos
+from chaffey_flight_reservation_sys import run_reservation_system_pos
 
 if __name__ == '__main__':
     run_reservation_system_pos()
-
